e3ebbc31e4d58b84aa307379394a922949d435c9
Katheleen Eduarda <katheleeneduarda123@gmail.com>
2024-10-30 14:12:58 -0300

    Criando a informação da despesa

--- scripts.js
+++ scripts.js
@@ -60,8 +60,23 @@ function expenseAdd(newExpense){
         expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
         expenseIcon.setAttribute("alt", newExpense.category_name)
 
+        //cria a info da despesa
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info")
+
+        //cria o nome da despesa
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
+
+        //cria a categoria da despesa
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.category_name
+
+        //adiciona name e category na div das informações da despesa
+        expenseInfo.append(expenseName, expenseCategory)
+
         // Adiciona as informações do item
-        expenseItem.append(expenseIcon)
+        expenseItem.append(expenseIcon, expenseInfo)
        
         //adiciona o item na lista
         expenseList.append(expenseItem)
